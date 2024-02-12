c6b455f9c63daaf39e9f2c35014c752a425b3091
Denys <denisdenis200524@gmail.com>
2024-02-12 08:52:40 +0200

    Initital commit

---
+++ src/main/java/ua/kibenmister/tgflashcardsbot/TgFlashcardsBotApplication.java
@@ -0,0 +1,13 @@
+package ua.kibenmister.tgflashcardsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgFlashcardsBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgFlashcardsBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kibenmister/tgflashcardsbot/TgFlashcardsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kibenmister.tgflashcardsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgFlashcardsBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
