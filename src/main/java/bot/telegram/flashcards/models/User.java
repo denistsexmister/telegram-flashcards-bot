c869532b325fb9c9311ddd4de579a61f5503a56b
kidenbar <danyakononenko14@gmail.com>
2024-02-13 21:15:02 +0200

    Modify entity User

--- src/main/java/bot/telegram/flashcards/models/User.java
+++ src/main/java/bot/telegram/flashcards/models/User.java
@@ -1,4 +1,18 @@
 package bot.telegram.flashcards.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
 }
