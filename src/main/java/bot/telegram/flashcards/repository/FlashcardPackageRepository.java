debc9773df3a961001ed4deaf7c7d4b0069721e3
Denys <denisdenis200524@gmail.com>
2024-02-29 20:04:07 +0200

    added Repository annotations to repo interfaces

--- src/main/java/bot/telegram/flashcards/repository/FlashcardPackageRepository.java
+++ src/main/java/bot/telegram/flashcards/repository/FlashcardPackageRepository.java
@@ -2,6 +2,8 @@ package bot.telegram.flashcards.repository;
 
 import bot.telegram.flashcards.models.FlashcardPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FlashcardPackageRepository extends CrudRepository<FlashcardPackage, Long> {
 }
--- src/main/java/bot/telegram/flashcards/repository/UserRepository.java
+++ src/main/java/bot/telegram/flashcards/repository/UserRepository.java
@@ -2,6 +2,8 @@ package bot.telegram.flashcards.repository;
 
 import bot.telegram.flashcards.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }
