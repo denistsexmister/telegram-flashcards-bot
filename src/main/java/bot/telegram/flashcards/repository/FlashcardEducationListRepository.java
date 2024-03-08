ea551ef9bfc82101070de1e6428ec063c55ea84f
Denys <denisdenis200524@gmail.com>
2024-03-08 11:45:34 +0200

    added new method to FlashcardEducationListRepository
    + added additional method countFlashcardEducationListByFlashcardEducationListPK_User
    that counts the number of flashcard in current learning process

--- src/main/java/bot/telegram/flashcards/repository/FlashcardEducationListRepository.java
+++ src/main/java/bot/telegram/flashcards/repository/FlashcardEducationListRepository.java
@@ -1,8 +1,10 @@
 package bot.telegram.flashcards.repository;
 
+import bot.telegram.flashcards.models.User;
 import bot.telegram.flashcards.models.temporary.FlashcardEducationList;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FlashcardEducationListRepository
         extends CrudRepository<FlashcardEducationList, FlashcardEducationList.FlashcardEducationListPK> {
+    long countFlashcardEducationListByFlashcardEducationListPK_User(User user);
 }
