1954e3af2ba8466f623a6aab3d4da99a85f3b964
kidenbar <danyakononenko14@gmail.com>
2024-03-23 21:17:44 +0200

    Check --amend

--- src/main/java/bot/telegram/flashcards/controller/ShowAllPackagesController.java
+++ src/main/java/bot/telegram/flashcards/controller/ShowAllPackagesController.java
@@ -4,9 +4,6 @@ import bot.telegram.flashcards.service.ShowAllPackagesService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Controller
@@ -14,18 +11,14 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class ShowAllPackagesController {
     private ShowAllPackagesService showAllPackagesService;
 
-    public EditMessageText showAllPackages(CallbackQuery callbackQuery) {
-        int messageId = ((Message) callbackQuery.getMessage()).getMessageId();
-        long chatId = callbackQuery.getMessage().getChatId();
+    public SendMessage showAllPackagesCommandReceived(Update update){
+        long chatId = update.getCallbackQuery().getMessage().getChatId();
 
-        return showAllPackagesService.getAllPackages(chatId, messageId);
+        return showAllPackagesService.getAllPackages(chatId);
     }
 
-//    public SendMessage showAllPackagesCommandReceived(Update update){
-//
-//        long chatId = update.getCallbackQuery().getMessage().getChatId();
-//        int messageId = update.getCallbackQuery().getMessage().getMessageId();
-//        return showAllPackagesService.getAllPackages(chatId, messageId);
-//    }
+//    TODO: make method, which will show description of chosen package, example startEducation(EducationController)
+// Check git commit --amend
+
 
 }
