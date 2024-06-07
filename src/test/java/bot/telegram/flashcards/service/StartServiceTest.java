3253f096c3064a3a88a32078bace61fa573272f1
kidenbar <danyakononenko14@gmail.com>
2024-06-07 22:41:57 +0300

    Work with tests

--- src/test/java/bot/telegram/flashcards/service/StartServiceTest.java
+++ src/test/java/bot/telegram/flashcards/service/StartServiceTest.java
@@ -1,80 +1,50 @@
 package bot.telegram.flashcards.service;
 
+import bot.telegram.flashcards.models.User;
 import bot.telegram.flashcards.repository.UserRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class StartServiceTest {
 
-    @Mock
-    private UserRepository userRepository;
     @InjectMocks
-    private StartService start;
+    private StartService startService;
 
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        start = new StartService(userRepository);
-        System.out.println("Setup start service was called");
-    }
-
-    @AfterEach
-    void tearDown() {
-        start = null;
-        System.out.println("Teardown start service was called");
+    @Test
+    void addUserIfNotInRepo() {
+        boolean result = startService.addUserIfNotInRepo(1L);
+        Assertions.assertThat(result).isFalse();
     }
 
     @Test
-    @DisplayName("It should add user if is in repo")
-    void should_add_user_if_is_in_repo() {
-        long chatId = 1L;
-
-        when(userRepository.existsById(chatId)).thenReturn(false);
-
-        // Act
-        boolean result = start.addUserIfNotInRepo(chatId);
-
-        // Assert
-        assertThat(result).isFalse();
-        verify(userRepository, times(1)).save(any());
-    }
-
-
-    @Test
-    @DisplayName("It should create welcome and guide messages")
-    void should_create_welcome_and_guide_messages() {
-        long chatId = 1L;
-        String userFirst = "Test";
-        assertThat(start.createWelcomeAndGuideMessages(chatId, userFirst)).isNotNull();
+    void createWelcomeAndGuideMessages() {
+        String sendMessage = startService.createWelcomeAndGuideMessages(1L, "test").toString();
+        Assertions.assertThat(sendMessage).isNotNull();
     }
 
     @Test
-    @DisplayName("It should create welcome message with get guide button")
-    void should_create_welcome_message_with_get_guide_button() {
-        long chatId = 1L;
-        String userFirst = "Test";
-        assertThat(start.createWelcomeMessageWithGetGuideButton(chatId, userFirst)).isNotNull();
+    void createWelcomeMessageWithGetGuideButton() {
+        String sendMessage = startService.createWelcomeMessageWithGetGuideButton(1L, "test").toString();
+        Assertions.assertThat(sendMessage).isNotNull();
     }
 
     @Test
-    @DisplayName("It should create guide message")
-    void should_create_guide_message() {
-        long chatId = 1L;
-
-        SendMessage result = start.createGuideMessage(chatId);
-        assertThat(result).isNotNull();
+    void createGuideMessage() {
+        String sendMessage = startService.createGuideMessage(1L).toString();
+        Assertions.assertThat(sendMessage).isNotNull();
     }
 }
