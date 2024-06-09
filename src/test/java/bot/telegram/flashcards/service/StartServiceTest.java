d1e832898d5ba5d1e7d17c5301d481e910a40b79
kidenbar <danyakononenko14@gmail.com>
2024-06-09 18:39:23 +0300

    Create new test UserServiceTest and refactor old test StartServiceTest

--- src/test/java/bot/telegram/flashcards/service/StartServiceTest.java
+++ src/test/java/bot/telegram/flashcards/service/StartServiceTest.java
@@ -3,6 +3,8 @@ package bot.telegram.flashcards.service;
 import bot.telegram.flashcards.models.User;
 import bot.telegram.flashcards.repository.UserRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,38 +15,93 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class StartServiceTest {
 
     @InjectMocks
     private StartService startService;
 
+    @Mock
+    private UserRepository userRepository;
+
+    private User mockUser;
+    private long userId;
+
+    @BeforeEach
+    void setUp() {
+        userId = 1L;
+        mockUser = new User();
+        mockUser.setId(userId);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Mockito.reset(userRepository); // Reset the mock
+    }
+
     @Test
-    void addUserIfNotInRepo() {
-        boolean result = startService.addUserIfNotInRepo(1L);
+    void addUserIfNotInRepo_userNotInRepo_savesUserAndReturnsFalse()  {
+
+        long chatId = 1L;
+        when(userRepository.existsById(chatId)).thenReturn(false);
+
+        boolean result = startService.addUserIfNotInRepo(chatId);
+
         Assertions.assertThat(result).isFalse();
+        verify(userRepository, Mockito.times(1)).save(mockUser);
     }
 
     @Test
-    void createWelcomeAndGuideMessages() {
-        String sendMessage = startService.createWelcomeAndGuideMessages(1L, "test").toString();
-        Assertions.assertThat(sendMessage).isNotNull();
+    void addUserIfNotInRepo_userInRepo_returnsTrue()  {
+        long chatId = 1L;
+        when(userRepository.existsById(chatId)).thenReturn(true);
+
+        boolean result = startService.addUserIfNotInRepo(chatId);
+
+        Assertions.assertThat(result).isTrue();
+        verify(userRepository, never()).save(mockUser);
     }
 
     @Test
-    void createWelcomeMessageWithGetGuideButton() {
-        String sendMessage = startService.createWelcomeMessageWithGetGuideButton(1L, "test").toString();
-        Assertions.assertThat(sendMessage).isNotNull();
+    void createWelcomeAndGuideMessages_validInput_returnsMessages() {
+        long chatId = 1L;
+        String userFirstName = "test";
+
+        List<SendMessage> messages = startService.createWelcomeAndGuideMessages(chatId, userFirstName);
+
+        Assertions.assertThat(messages).isNotNull();
+        Assertions.assertThat(messages).hasSize(2);
+        Assertions.assertThat(messages.get(0).getText()).contains(userFirstName);
+        Assertions.assertThat(messages.get(1).getText()).isEqualTo("GUIDE_MESSAGE");
+    }
+
+    @Test
+    void createWelcomeMessageWithGetGuideButton_validInput_returnsMessage() {
+        long chatId = 1L;
+        String userFirstName = "test";
+
+        List<SendMessage> messages = startService.createWelcomeAndGuideMessages(chatId, userFirstName);
+
+        Assertions.assertThat(messages).isNotNull();
+        Assertions.assertThat(messages).hasSize(2);
+        Assertions.assertThat(messages.get(0).getText()).contains(userFirstName);
+        Assertions.assertThat(messages.get(0).getReplyMarkup()).isNotNull();
     }
 
     @Test
     void createGuideMessage() {
-        String sendMessage = startService.createGuideMessage(1L).toString();
-        Assertions.assertThat(sendMessage).isNotNull();
+        long chatId = 1L;
+
+        SendMessage message = startService.createGuideMessage(chatId);
+
+        Assertions.assertThat(message).isNotNull();
+        Assertions.assertThat(message.getText()).isEqualTo("GUIDE_MESSAGE");
     }
 }
